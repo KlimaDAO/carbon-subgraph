d4bb7b067293547d77c36a6107a28decb1aa9a8c
cujowolf <cujowolf12@gmail.com>
2022-02-03 21:13:37 -0600

    Add Moss MCO2 Tokens
    Add in totals for the MCO2 supply on Polygon. While the underlying methodologies and specifics of these tokens are not available, still useful to track the current supply movement.

---
+++ src/MossCarbon.ts
@@ -0,0 +1,51 @@
+import { Address } from '@graphprotocol/graph-ts'
+
+import { ERC20, Transfer } from '../generated/MossCarbon/ERC20'
+
+import { toDecimal } from './utils/Decimals'
+import { loadOrCreateCarbonOffset } from './utils/CarbonOffsets'
+import { loadOrCreateTransaction } from './utils/Transactions'
+import { loadOrCreateBridge } from './utils/Bridge'
+import { Bridge } from '../generated/schema'
+import { loadOrCreateRetire } from './utils/Retire'
+
+export function handleTransfer(event: Transfer): void {
+
+    let transaction = loadOrCreateTransaction(event.transaction, event.block)
+    let offsetERC20 = ERC20.bind(event.address)
+
+    let carbonOffset = loadOrCreateCarbonOffset(transaction, event.address, 'Moss', 'Verra')
+
+
+    // Handle Fractionalizing (Mints from briding)
+    if (event.params.from == Address.fromString('0x0000000000000000000000000000000000000000')) {
+
+        let bridge = loadOrCreateBridge(carbonOffset, transaction)
+        bridge.value = toDecimal(event.params.value, 18)
+        bridge.bridger = event.params.to.toHexString()
+
+        bridge.save()
+
+        carbonOffset.totalBridged = carbonOffset.totalBridged.plus(toDecimal(event.params.value, 18))
+
+        //carbonOffset.bridges.push(bridge.id)
+    }
+
+    // Handle Retirements
+    if (event.params.to == Address.fromString('0x0000000000000000000000000000000000000000')) {
+
+        let bridge = loadOrCreateBridge(carbonOffset, transaction)
+        bridge.value = toDecimal(event.params.value, 18)
+        bridge.bridger = event.params.from.toHexString()
+
+        bridge.save()
+
+        carbonOffset.totalBridged = carbonOffset.totalBridged.minus(toDecimal(event.params.value, 18))
+        //carbonOffset.retirements.push(retire.id)
+    }
+
+    carbonOffset.currentSupply = toDecimal(offsetERC20.totalSupply(), 18)
+    carbonOffset.lastUpdate = transaction.timestamp
+
+    carbonOffset.save()
+}
