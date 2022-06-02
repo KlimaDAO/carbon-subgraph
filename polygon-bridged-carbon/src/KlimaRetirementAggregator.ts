421fa77147d0b9d7357985c693f93e226f802d10
milos1991 <15165767+milos1991@users.noreply.github.com>
2022-06-02 02:09:52 +0200

    aggregated-retirements:  (#13)
    * aggregated-retirements:
    - Added token and fee fields within "Retire" entity;
    - Added "DailyRetirement" entity that aggregates daily retirements for a particular offset
    - Updated graph-ts to 0.26.0 (now it is the same as in klima subgraph) since i had issues with deploying with the newest graph-cli (required graph-ts version >0.25.0)
    
    * aggregated-retirements: Fixed a bug with fee calculation for toucan and c3
    
    * aggregated-retirements: Renamed DailyRetirement to DailyKlimaRetirement
    
    * aggregated-retirements: Updated package-lock.json
    
    Co-authored-by: Sekula <sekuloskimilos@gmail.com>

--- polygon-bridged-carbon/src/KlimaRetirementAggregator.ts
+++ polygon-bridged-carbon/src/KlimaRetirementAggregator.ts
@@ -1,19 +1,28 @@
-import { MossRetired } from "../generated/RetireMossCarbon/RetireMossCarbon"
-import { ToucanRetired } from "../generated/RetireToucanCarbon/RetireToucanCarbon"
-import { C3Retired } from "../generated/RetireC3Carbon/RetireC3Carbon"
+import { MossRetired, RetireMossCarbon } from "../generated/RetireMossCarbon/RetireMossCarbon"
+import { RetireToucanCarbon, ToucanRetired } from "../generated/RetireToucanCarbon/RetireToucanCarbon"
+import { C3Retired, RetireC3Carbon } from "../generated/RetireC3Carbon/RetireC3Carbon"
+import { KlimaRetire, DailyKlimaRetirement } from "../generated/schema";
+import * as constants from "../../lib/utils/Constants";
+
 import { KlimaCarbonRetirements } from "../generated/RetireC3Carbon/KlimaCarbonRetirements"
 import { loadOrCreateCarbonOffset } from "./utils/CarbonOffsets"
+import { loadOrCreateDailyKlimaRetirement as loadOrCreateDailyKlimaRetirement } from "./utils/DailyKlimaRetirement"
+import { getTokenFromPoolAddress } from "./utils/Token"
 import { toDecimal } from "../../lib/utils/Decimals"
+import { dayFromTimestamp } from "../../lib/utils/Dates"
 import { loadOrCreateTransaction } from "./utils/Transactions"
 import { loadOrCreateKlimaRetire } from "./utils/KlimaRetire"
-import { Address, BigInt } from "@graphprotocol/graph-ts"
+import { Address, BigDecimal, BigInt } from "@graphprotocol/graph-ts"
 
 export function handleMossRetired(event: MossRetired): void {
 
     let transaction = loadOrCreateTransaction(event.transaction, event.block)
     let offset = loadOrCreateCarbonOffset(transaction, event.params.carbonPool, 'Moss', 'Verra')
     let retire = loadOrCreateKlimaRetire(offset, transaction)
-    let klimaRetirements = KlimaCarbonRetirements.bind(Address.fromString('0xac298cd34559b9acfaedea8344a977eceff1c0fd'))
+    let klimaRetirements = KlimaCarbonRetirements.bind(Address.fromString(constants.KLIMA_CARBON_RETIREMENTS_CONTRACT))
+
+    const token = getTokenFromPoolAddress(event.params.carbonPool)
+    const fee = RetireMossCarbon.bind(event.address).feeAmount().toBigDecimal()
 
     retire.retiringAddress = event.params.retiringAddress.toHexString()
     retire.beneficiaryAddress = event.params.beneficiaryAddress.toHexString()
@@ -23,8 +32,12 @@ export function handleMossRetired(event: MossRetired): void {
     retire.retirementMessage = event.params.retirementMessage
 
     retire.pool = event.params.carbonPool.toHexString()
+    retire.token = token
     retire.amount = toDecimal(event.params.retiredAmount)
+    retire.feeAmount = retire.amount.times(fee.div(BigDecimal.fromString("1000")))
 
+    const dailyRetirement = generateDailyKlimaRetirement(retire)
+    dailyRetirement.save()
     // TODO: add separate handler for specific retirements
     // retire.specific = true
 
@@ -35,7 +48,10 @@ export function handleToucanRetired(event: ToucanRetired): void {
     let transaction = loadOrCreateTransaction(event.transaction, event.block)
     let offset = loadOrCreateCarbonOffset(transaction, event.params.carbonToken, 'Toucan', 'Verra')
     let retire = loadOrCreateKlimaRetire(offset, transaction)
-    let klimaRetirements = KlimaCarbonRetirements.bind(Address.fromString('0xac298cd34559b9acfaedea8344a977eceff1c0fd'))
+    let klimaRetirements = KlimaCarbonRetirements.bind(Address.fromString(constants.KLIMA_CARBON_RETIREMENTS_CONTRACT))
+
+    const token = getTokenFromPoolAddress(event.params.carbonPool)
+    const fee = RetireToucanCarbon.bind(event.address).feeAmount().toBigDecimal()
 
     retire.retiringAddress = event.params.retiringAddress.toHexString()
     retire.beneficiaryAddress = event.params.beneficiaryAddress.toHexString()
@@ -45,8 +61,12 @@ export function handleToucanRetired(event: ToucanRetired): void {
     retire.retirementMessage = event.params.retirementMessage
 
     retire.pool = event.params.carbonPool.toHexString()
+    retire.token = token
     retire.amount = toDecimal(event.params.retiredAmount)
+    retire.feeAmount = retire.amount.times(fee.div(BigDecimal.fromString("1000")))
 
+    const dailyRetirement = generateDailyKlimaRetirement(retire)
+    dailyRetirement.save()
     // TODO: add separate handler for specific retirements
     // retire.specific = true
 
@@ -57,7 +77,10 @@ export function handleC3Retired(event: C3Retired): void {
     let transaction = loadOrCreateTransaction(event.transaction, event.block)
     let offset = loadOrCreateCarbonOffset(transaction, event.params.carbonToken, 'C3', 'Verra')
     let retire = loadOrCreateKlimaRetire(offset, transaction)
-    let klimaRetirements = KlimaCarbonRetirements.bind(Address.fromString('0xac298cd34559b9acfaedea8344a977eceff1c0fd'))
+    let klimaRetirements = KlimaCarbonRetirements.bind(Address.fromString(constants.KLIMA_CARBON_RETIREMENTS_CONTRACT))
+
+    const token = getTokenFromPoolAddress(event.params.carbonPool)
+    const fee = RetireC3Carbon.bind(event.address).feeAmount().toBigDecimal()
 
     retire.retiringAddress = event.params.retiringAddress.toHexString()
     retire.beneficiaryAddress = event.params.beneficiaryAddress.toHexString()
@@ -67,10 +90,30 @@ export function handleC3Retired(event: C3Retired): void {
     retire.retirementMessage = event.params.retirementMessage
 
     retire.pool = event.params.carbonPool.toHexString()
+    retire.token = token
     retire.amount = toDecimal(event.params.retiredAmount)
+    retire.feeAmount = retire.amount.times(fee.div(BigDecimal.fromString("1000")))
 
+    const dailyKlimaRetirement = generateDailyKlimaRetirement(retire)
+    dailyKlimaRetirement.save()
     // TODO: add separate handler for specific retirements
     // retire.specific = true
 
     retire.save()
+}
+
+function generateDailyKlimaRetirement(klimaRetire: KlimaRetire): DailyKlimaRetirement {
+
+    const dayTimestamp = dayFromTimestamp(klimaRetire.timestamp)
+    const id = dayTimestamp + klimaRetire.token
+
+    const dailyKlimaRetirement = loadOrCreateDailyKlimaRetirement(id)
+    dailyKlimaRetirement.amount = dailyKlimaRetirement.amount.plus(klimaRetire.amount)
+    dailyKlimaRetirement.feeAmount = dailyKlimaRetirement.feeAmount.plus(klimaRetire.feeAmount)
+    dailyKlimaRetirement.offset = klimaRetire.offset
+    dailyKlimaRetirement.pool = klimaRetire.pool
+    dailyKlimaRetirement.token = klimaRetire.token
+    dailyKlimaRetirement.timestamp = BigInt.fromString(dayTimestamp)
+
+    return dailyKlimaRetirement
 }
